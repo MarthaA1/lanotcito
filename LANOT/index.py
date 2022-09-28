08c56ac2618f8ce5c4efb745b8309c9a563674f7
Oswavg <osssdb23@gmail.com>
2022-09-28 03:32:12 -0500

    Ultima prueba 2

--- LANOT/index.py
+++ LANOT/index.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -12,4 +11,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=8000, debug=True) 
+    app.run( host='0.0.0.0', port=8000, debug=True)
