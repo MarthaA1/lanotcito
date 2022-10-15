a23bf28f103e75c8204cf6389b0e704a1a444eea
Oswavg <osssdb23@gmail.com>
2022-10-14 21:07:27 -0500

    Muestra de funciones en los js

---
+++ LANOT/static/js/funciones_gee.js
@@ -0,0 +1,4 @@
+
+// script de funciones de gee-API
+
+window.alert("funciones_gee.js")
---
+++ LANOT/static/js/mapa_funcion.js
@@ -0,0 +1,4 @@
+
+// script de funciomes y configuración de la vista y funciones de los elementos del mapa
+
+window.alert("mapa_funcion.js")
